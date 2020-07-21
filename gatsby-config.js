83fa1658db0183ecdba052429f1df378cf7a24ec
huequica <harumaki1225@gmail.com>
2020-07-21 14:49:12 +0900

    change siteMetadatas

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `huequica.xyz`,
+    description: `huequica's Portfolio Site.`,
+    author: `@huequica`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
