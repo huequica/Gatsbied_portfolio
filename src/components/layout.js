381f6e55174906fe52321296e102167e406006d8
huequica <harumaki1225@gmail.com>
2020-07-21 16:34:56 +0900

    insert style import

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from "gatsby";
+import './scss/base.scss';
 
 const NavBar = (props) => {
   return (
