ee83e78f71f48d2cd16143fe7a9e4c7048f616dd
huequica <harumaki1225@gmail.com>
2020-07-21 16:40:42 +0900

    devide navbar component from layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,30 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from "gatsby";
+
 import './scss/base.scss';
-
-const NavBar = (props) => {
-  return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          { props.siteTitle }
-        </a>
-
-        { /* eslint-disable-next-line jsx-a11y/anchor-is-valid */ }
-        <a role="button" className="navbar-burger"
-          aria-label="menu" aria-expanded="false">
-
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        
-        </a>
-      </div>
-    </nav>
-  );
-};
-
+import NavBar from './navbar';
 
 const Layout = (props) => {
   const contents = props.children;
@@ -48,7 +27,7 @@ const Layout = (props) => {
 };
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 };
 
 export default Layout;
