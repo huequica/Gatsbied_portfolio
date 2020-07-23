7b863a07a8ecbbd7324595cc96900d0097785f02
huequica <harumaki1225@gmail.com>
2020-07-24 02:49:11 +0900

    create heroStyle decider method

--- src/bulmaElements/layout/hero.js
+++ src/bulmaElements/layout/hero.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * hero Style decider
+ *  @param {props} componentOrder component props
+ *  @param {array<string>} baseHeroStyles heroStyle base definition
+ *
+ *  @return {array<string>} decided heroStyles
+ */
+const heroStyleDecide = (componentOrder, baseHeroStyles) => {
+  const heroStyles = baseHeroStyles;
+
+  // subtleGradient is enable?
+  if(componentOrder.subtleGradient === true) heroStyles.push('is-bold');
+
+  // color style definition(e.g. `is-primary`, `is-success`)
+  if(componentOrder.colorStyle !== undefined) heroStyles.push(componentOrder.colorStyle);
+
+  // hero size definition(e.g. `is-medium`, `is-fullheight`, `is-fullheight-with-navbar`)
+  if(componentOrder.size !== undefined) heroStyles.push(componentOrder.size);
+
+  return heroStyles;
+};
+
 const Hero = (props) => {
   const contents = props.children;
   const [heroStyle, setHeroStyle] = React.useState('hero');
@@ -16,8 +38,15 @@ const Hero = (props) => {
   );
 };
 
+Hero.defaultProps = {
+  subtleGradient: false
+};
+
 Hero.propTypes = {
+  // Not Required
   children: PropTypes.node,
+  subtleGradient: PropTypes.bool,
+  colorStyle: PropTypes.string
 };
 
 export { Hero };
