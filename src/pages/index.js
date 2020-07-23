38abc8bd55383eaefd806d209a5ec962257c8744
huequica <harumaki1225@gmail.com>
2020-07-24 03:16:26 +0900

    add Hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,9 @@ import * as Bulma from '../bulmaElements/index';
 
 const IndexPage = () => (
   <Layout>
-    <Bulma.elements.Title>Haru Yakumo's Portfolio Site</Bulma.elements.Title>
+    <Bulma.layout.Hero colorStyle='is-info' size='is-fullheight' subtleGradient={ true }>
+      <Bulma.elements.Title>Haru Yakumo's Portfolio Site</Bulma.elements.Title>
+    </Bulma.layout.Hero>
   </Layout>
 );
 
