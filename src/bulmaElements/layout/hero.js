c3ca7b4f3b04a8fc18f7518e84f26ce415907c6d
huequica <harumaki1225@gmail.com>
2020-07-24 02:08:48 +0900

    move hero style definition to `heroStyle` state

--- src/bulmaElements/layout/hero.js
+++ src/bulmaElements/layout/hero.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 
 const Hero = (props) => {
   const contents = props.children;
+  const [heroStyle, setHeroStyle] = React.useState('hero');
 
   return (
-    <section className={ `hero` }>
+    <section className={ heroStyle }>
       <div className='hero-body'>
         <div className='container'>
           { contents }
