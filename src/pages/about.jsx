8fefd16e934f69890b546d7af2cc6c070a9791a6
huequica <harumaki1225@gmail.com>
2020-07-29 16:38:01 +0900

    create About page

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '@layout';
+import * as Bulma from '@bulma';
+
+const About = () => (
+  <Layout>
+    <Bulma.layout.Hero colorStyle='is-primary'>
+      <Bulma.elements.Title>/About</Bulma.elements.Title>
+    </Bulma.layout.Hero>
+  </Layout>
+);
+
+export default About;
