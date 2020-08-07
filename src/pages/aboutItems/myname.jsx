b8e2e8d04c388051d54023f29518ad0b7262d3b9
huequica <harumaki1225@gmail.com>
2020-08-07 11:26:04 +0900

    create Name sections render

---
+++ src/pages/aboutItems/myname.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MyName = () => {
+  return (
+    <div className='columns'>
+      <div className='column'>
+        hoge
+
+      </div>
+      <div className='column'>
+        fuga
+
+      </div>
+    </div>
+  );
+};
+
+export default MyName;
