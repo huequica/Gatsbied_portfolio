1bcd499101e344555319ec0ca67fee74a700377f
huequica <harumaki1225@gmail.com>
2020-08-07 11:26:47 +0900

    insert name section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Layout from '@layout';
 import * as Bulma from '@bulma';
 
+import MyName from './aboutItems/myname';
+
+
 const About = () => (
   <Layout>
     <Bulma.layout.Hero colorStyle='is-primary'>
       <Bulma.elements.Title>/About</Bulma.elements.Title>
     </Bulma.layout.Hero>
 
-    <Bulma.elements.Title subTitle>{ 'huequica.xyz' }</Bulma.elements.Title>
+    <Bulma.elements.Title subTitle>{ 'huequica.xyzについて' }</Bulma.elements.Title>
+
+    Haru Yakumoが個人で運営しているポートフォリオみたいなサイトです。<br />
+    GatsbyJSによって記述されています。
+
+    <MyName />
   </Layout>
 );
 
