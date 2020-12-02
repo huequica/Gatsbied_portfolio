9410d42e7a4e55e6ac16e4e92cc5615e9f7c5a73
huequica <harumaki1225@gmail.com>
2020-12-02 16:40:33 +0900

    write Document

---
+++ src/pages/aboutItems/table.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Table = () => {
+  return (
+    <table className='table is-bordered'>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Haru Yakumo</td>
+        </tr>
+        
+        <tr>
+          <td>RealName</td>
+          <td>Harumi Ono</td>
+        </tr>
+
+        <tr>
+          <td>Live</td>
+          <td>Toyota, Aichi</td>
+        </tr>
+        <tr>
+          <td>Like</td>
+          <td>MusicGame, Web Dev(FrontEnd), DDLC</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
