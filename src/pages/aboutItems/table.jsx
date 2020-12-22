4cf9d61fb35b3fc7fae0a1f0c53af320314205e8
huequica <harumaki1225@gmail.com>
2020-12-23 01:48:15 +0900

    delete commentout module

--- src/pages/aboutItems/table.jsx
+++ src/pages/aboutItems/table.jsx
@@ -1,35 +1,6 @@
 import React from 'react';
 import * as Bulma from '@bulma';
 
-// const Table = () => {
-//   return (
-//     <table className='table is-bordered'>
-//       <tbody>
-//         <tr>
-//           <td>Name</td>
-//           <td>Haru Yakumo</td>
-//         </tr>
-        
-//         <tr>
-//           <td>RealName</td>
-//           <td>Harumi Ono</td>
-//         </tr>
-
-//         <tr>
-//           <td>Live</td>
-//           <td>Toyota, Aichi</td>
-//         </tr>
-
-//         { /* TODO: あとでここはそれぞれ別ページのリンクにしたいかも */ }
-//         <tr>
-//           <td>Like</td>
-//           <td>MusicGame, Web Dev(FrontEnd), DDLC</td>
-//         </tr>
-//       </tbody>
-//     </table>
-//   );
-// };
-
 // TitleModule alias
 const Title = Bulma.elements.Title;
 
