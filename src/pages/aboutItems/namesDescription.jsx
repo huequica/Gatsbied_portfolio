63a7dda3015631befffe800dbebe577113d3f6a6
huequica <harumaki1225@gmail.com>
2020-12-23 01:52:50 +0900

    Fix Indent

--- src/pages/aboutItems/namesDescription.jsx
+++ src/pages/aboutItems/namesDescription.jsx
@@ -7,13 +7,13 @@ const Title = Bulma.elements.Title;
 const NamesDescription = () => {
   return (
     <>
-      <Title is={ 2 }># Name</Title>
+      <Title is={ 2 }         ># Name</Title>
       <Title subTitle is={ 4 }>Haru Yakumo</Title>
-      <Title is={ 2 }># RealName</Title>
+      <Title is={ 2 }         ># RealName</Title>
       <Title subTitle is={ 4 }>Harumi Ono</Title>
-      <Title is={ 2 }># Live</Title>
+      <Title is={ 2 }         ># Live</Title>
       <Title subTitle is={ 4 }>Toyota, Aichi</Title>
-      <Title is={ 2 }># Like</Title>
+      <Title is={ 2 }         ># Like</Title>
       <Title subTitle is={ 4 }>MusicGame, Web Dev(FrontEnd), DDLC</Title>
     </>
   );
