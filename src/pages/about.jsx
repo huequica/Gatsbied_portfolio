250b901fccd8ee4287da8e16556ffe143962c0e4
huequica <harumaki1225@gmail.com>
2021-02-07 00:43:30 +0900

    fix title text

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -8,7 +8,7 @@ import MyName from './aboutItems/myname';
 const About = () => (
   <Layout>
     <Bulma.layout.Hero colorStyle='is-primary'>
-      <Bulma.elements.Title>/About</Bulma.elements.Title>
+      <Bulma.elements.Title>/about</Bulma.elements.Title>
     </Bulma.layout.Hero>
 
     <Bulma.elements.Title subTitle>{ 'huequica.xyzについて' }</Bulma.elements.Title>
