43774e91866137aa4374944c557a060456a77b8d
huequica <harumaki1225@gmail.com>
2021-06-01 12:47:35 +0900

    contain to box

--- src/pages/works.jsx
+++ src/pages/works.jsx
@@ -14,33 +14,35 @@ const Works = () => (
     <Bulma.layout.Hero colorStyle='is-primary'>
       <Bulma.elements.Title>/worsks</Bulma.elements.Title>
     </Bulma.layout.Hero>
-    
-    <Bulma.Column Parent>
-      <Bulma.Column size='is-3'>
-        <Image filename='hikakinsymmetry.png' alt='@hikakinSymmetry'/>
+    <div className='box'>
+      <Bulma.Column Parent>
+        <Bulma.Column size='is-3'>
+          <Image filename='hikakinsymmetry.png' alt='@hikakinSymmetry'/>
+        </Bulma.Column>
+
+        <Bulma.Column>
+          <Bulma.elements.Title subTitle>
+            # ヒカキンシンメトリーbot(新)
+          </Bulma.elements.Title>
+
+          <Intent link='https://twitter.com/hikakin_sym'>ヒカキンシンメトリーbot</Intent>の稼働が止まっていたので再実装して公開したbot<br />
+          百聞は一見に如かずなので一回Twitterを見てほしい
+          
+          <div className='buttons are-medium'>
+            <Intent link='https://github.com/HIKAKINSymmetry/nodeBot'>
+              <Bulma.elements.Button isRounded color='is-dark'>GitHub</Bulma.elements.Button>
+            </Intent>
+
+            <Intent link='https://twitter.com/hikakinsymmetry'>
+              <Bulma.elements.Button isRounded color='is-success'>Twitter</Bulma.elements.Button>
+            </Intent>
+          </div>
+
+        </Bulma.Column>
+
       </Bulma.Column>
 
-      <Bulma.Column>
-        <Bulma.elements.Title subTitle>
-          # ヒカキンシンメトリーbot(新)
-        </Bulma.elements.Title>
-
-        <Intent link='https://twitter.com/hikakin_sym'>ヒカキンシンメトリーbot</Intent>の稼働が止まっていたので再実装して公開したbot<br />
-        百聞は一見に如かずなので一回Twitterを見てほしい
-        
-        <div className='buttons are-medium'>
-          <Intent link='https://github.com/HIKAKINSymmetry/nodeBot'>
-            <Bulma.elements.Button isRounded color='is-dark'>GitHub</Bulma.elements.Button>
-          </Intent>
-
-          <Intent link='https://twitter.com/hikakinsymmetry'>
-            <Bulma.elements.Button isRounded color='is-success'>Twitter</Bulma.elements.Button>
-          </Intent>
-        </div>
-
-      </Bulma.Column>
-
-    </Bulma.Column>
+    </div>
   </Layout>
 );
 
